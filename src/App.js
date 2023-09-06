63b6f2a6e65b91e7e98da4f9dd0abf8f8affa6a3
thugperfect <meenamani9444@gmail.com>
2023-09-06 23:04:00 +0530

    add json and local storage feature

--- src/App.js
+++ src/App.js
@@ -3,27 +3,10 @@ import './App.css';
 import Header from './Components/Header';
 import Body from './Components/Body';
 import { useState } from 'react';
+import Setinput from './Components/Setinput';
 
 function App() {
-  const [todo,setTodo] = useState([
-    {
-      id:1,
-      checked:true,
-      item:"Complete ToDO APP"
-    },{
-      id:2,
-      checked:false,
-      item:"Optimise Movie_Fetch"
-    },{
-      id:3,
-      checked:false,
-      item:"Optimise Java_DSA"
-    },{
-      id:4,
-      checked:false,
-      item:"Complete React YT video"
-    }
-  ])
+  const [todo,setTodo] = useState(JSON.parse(localStorage.getItem('todoList')) || [])
   const handleChecked = (key)=>{
     const addCheck = todo.map(k=>
       (k.id === key)? {...k,checked:!k.checked}:k
@@ -38,10 +21,24 @@ function App() {
     localStorage.setItem("todoList",JSON.stringify(changeList))
     
   }
+  const [newtodo,setNewTodo] = useState("")
+  
+  const addItem = (key)=>{
+    const id = todo.length+1
+    const newItem = {id:id,checked:false,item:key}
+    const listItem = [...todo,newItem]
+    localStorage.setItem("todoList",JSON.stringify(listItem))
+    setTodo(listItem)    
+  }
 
   return (
     <div className="container mx-auto w-4/5  min-h-[100vh]  bg-black  outline-1 outline-slate-600">
       <Header/>
+      <Setinput
+      setNewTodo={setNewTodo}
+      addItem={addItem}
+      newtodo={newtodo}
+      />
       <Body
       todo={todo}
       deleteTodo={deleteTodo}
