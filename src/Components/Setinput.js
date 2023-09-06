c4917e548d3770c66db633a050c447045dc9cb2b
thugperfect <meenamani9444@gmail.com>
2023-09-06 23:08:15 +0530

    prevent default

--- src/Components/Setinput.js
+++ src/Components/Setinput.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 const Setinput = ({setNewTodo,addItem,newtodo}) => {
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
   return (
-    <div className='w-full flex justify-center mt-3 '>
+    <form className='w-full flex justify-center mt-3 ' onSubmit={(e)=>handleSubmit(e)}>
         <div className='w-4/5 flex items-center'>
         <input type="text" onChange={(e)=>setNewTodo(e.target.value)} className='w-full text-gray-600 outline-none px-5 h-[40px] text-xl' placeholder='Add TODO' />
         <button onClick={()=>addItem(newtodo)} className='h-[40px] w-[80px] bg-red-500 hover:bg-green-700'>ADD</button>
         </div>
-        
-    </div>
+    </form>
   )
 }
 
